596146cfba8a8b7f983e0902c8d5dd4a6defb43f
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-09-27 17:43:45 -0300

    Read expression from STDIN and prepare it for parsing.

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,43 @@
-// Program to read an parse mathematical expressions from
-// command line.
+/* Program to read an parse mathematical expressions from
+ * command line. */
 use std::io;
-use std::iter::zip;
+use std::collections::HashMap;
 
 fn main() {
+    let operators = ['+', '-', '*', '/'];
     println!("> ");
-    // let expr = read_string();
-    parse(String::from("1+2"));
+    let mut expr = read_expr(&operators);
 }
 
-// Read string from STDIN
-fn read_string() -> String {
+// Read expression from STDIN
+fn read_expr(operators: &[char]) -> Vec<String> { 
+    // Read input string
     let mut str_input = String::new();
     io::stdin().read_line(&mut str_input);
-    str_input
-}
+    
+    // Prepare expression
+    let expr = str_input.split_whitespace().collect::<String>();
+    let mut expression = Vec::<String>::new();
+    let mut operand = String::new();
 
-// Parse expression
-fn parse(expr: String) -> f64 {
-    let expr_vector = prepare_expr(expr);
-
-    println!("{:?}", expr_vector);
-
-    0.0
-}
-
-fn prepare_expr(expr: String) -> Vec<String> {
-    let expr: String = expr.split(" ").collect(); // Remove spaces
-    let mut operators = Vec::new();
     for c in expr.chars() {
-        match c {
-            '+' | '-' | '*' | '/' => operators.push(c),
-            _ => ()
+        if c.is_numeric() {
+            operand.push(c);
+        } else {
+            if operators[..].contains(&c) {
+                expression.push(operand.clone());
+                expression.push(c.to_string());
+                operand = String::new();
+            } else {
+                panic!("Invalid operator.");
+            }
         }
     }
-    let operands = expr.split(&['+', '-', '*', '/'][..]);
-    
-    let mut expr_vector = Vec::new();
-    for i in zip(operands, operators) {
-        expr_vector.push(i.0.to_string());
-        expr_vector.push(i.1.to_string());
-    }
+    expression.push(operand.clone());
 
-    expr_vector
+    expression
 }
+
+// Add two numbers
+// fn add<T>(a: T, b: T) -> T {
+// }

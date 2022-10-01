c817957f908038dde0662c2d109f98ac634a20c2
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-10-01 18:04:55 -0300

    Convert expression string to vector.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 use std::io;
 use std::collections::HashMap;
 
-const SYMBOLS: [(&str, u8); 7] = [
+const OPERATORS_PRECEDENCE: [(&str, u8); 7] = [
     ("+", 1),
     ("-", 1),
     ("*", 2),
@@ -13,8 +13,7 @@ const SYMBOLS: [(&str, u8); 7] = [
     (")", 3),
     ("**", 4),
 ];
-
-const OPERATORS_CHARS: [char; 4] = ['+', '-', '*', '/'];
+const SYMBOLS: [&str; 7] = ["+", "-", "*", "/", "**", "(", ")"];
 
 
 // Read string from STDIN
@@ -26,14 +25,14 @@ fn read_string() -> String {
 
 
 // Check if the expression string is valid
-fn is_valid(expr_string: String) -> bool {
+fn is_valid(expr_string: &String) -> bool {
     // Remove whitespaces
     let temp_expr_string: String = expr_string.split_whitespace().collect();
 
     // Validate characters
     for c in temp_expr_string.chars() {
         if !c.is_numeric() {
-            if !OPERATORS_CHARS.contains(&c) {
+            if !SYMBOLS.contains(&c.to_string().as_str()) {
                 return false;
             }
         }
@@ -93,7 +92,52 @@ fn is_valid(expr_string: String) -> bool {
 }
 
 
+// Convert expression string to vector.
+fn convert_to_vector(expr_string: &String) -> Vec<String> {
+    // Remove spaces
+    let mut temp_expr_string = expr_string
+        .split_whitespace()
+        .into_iter()
+        .collect::<String>();
+
+    // Separate operands from operators
+    let mut expr_vector = Vec::<String>::new();
+    let mut operand = String::new();
+    let mut operator = String::new();
+
+    for c in temp_expr_string.chars() {
+        if c.is_numeric() {
+            if !operator.is_empty() {
+                expr_vector.push(operator);
+                operator = String::new();
+            }
+            operand.push(c);
+        } else {
+            if !operand.is_empty() {
+                expr_vector.push(operand);
+                operand = String::new();
+            }
+            operator.push(c);
+        }
+    }
+    expr_vector.push(operand);
+
+    expr_vector
+}
+
+
+// Convert expression string to prefix notation.
+// fn convert_to_prefix_notation(expr_string: String) -> Vec<String> {
+//     let mut expr: Vec<String> = Vec::new();
+//
+//     expr
+// }
+
+
 fn main() {
     let expr_string = read_string();
-    println!("{:?}", is_valid(expr_string));
+    if !is_valid(&expr_string) {
+        panic!("Expressão inválida.");
+    }
+    println!("{:?}", convert_to_vector(&expr_string));
 }
